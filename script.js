a82356aab382060f709afeb950c822f0eea763b6
ShannonB320 <84356531+ShannonB320@users.noreply.github.com>
2021-07-26 20:46:24 -0500

    completed Fetch and JSON Studio

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+ 
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(responseJson) {
+            responseJson.sort((a,b) => {
+                return (b.hoursInSpace - a.hoursInSpace);
+            });
+
+            let countSection = document.getElementById("astronautCount");
+            countSection.innerHTML = `Data on ${responseJson.length} astronauts available.`
+
+            let astronautSection = document.getElementById("container");
+            
+            function colorDecision(activeSetting) {
+                let colorChoice = "";
+                if (activeSetting === true) {
+                    colorChoice = "currentlyActive";
+                };
+                return colorChoice;
+            }    
+
+            for (let i=0; i < responseJson.length; i++) {
+                astronautSection.innerHTML += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${responseJson[i].firstName} ${responseJson[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in Space: ${responseJson[i].hoursInSpace}</li>
+                                <li class="${colorDecision(responseJson[i].active)}">Active: ${responseJson[i].active}</li>
+                                <li>Skills: ${responseJson[i].skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src="${responseJson[i].picture}"></img>
+                    </div>
+                `;
+            };
+        });
+    });
+
+
+});
